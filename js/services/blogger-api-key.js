5e24e5725e0be86fbe8daf2c317ac9c238d4cc10
Ruben Htun <rubenhtun2003@gmail.com>
2026-08-24 13:25:36 +0700

    fix: give correct path for articles api key

--- js/services/blogger-api-key.js
+++
@@ -1,7 +0,0 @@
-/**
- * @file blogger-api-key.js
- * @description Contains the Blogger API key used for fetching articles.
- * @module services/blogger-api-key
- */
-import BLOGGER_API_KEY from "./bloggerapikey.js";
-export default BLOGGER_API_KEY;
